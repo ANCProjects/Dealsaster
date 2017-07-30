3284a7c9e3b14bf3bdaa250a1fb0b1156c152182
dewunmi <simonadewunmi@gmail.com>
2017-07-30 23:43:42 +0100

    Added todo list

---
+++ app/src/main/java/io/github/dewunmi/dealsaster/LoginActivity.java
@@ -0,0 +1,16 @@
+package io.github.dewunmi.dealsaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //TODO 1: Add firebase Login authentication
+
+    }
+}
--- app/src/main/java/io/github/dewunmi/dealsaster/MainActivity.java
+++ app/src/main/java/io/github/dewunmi/dealsaster/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO 2: Add Tab layout and view pager to enable easy swipe around activities/fragments
+        //TODO 3: Add Chat functionality to the app
+        //TODO 4: Add list of trusted friends to contact in case of an emergency
+        //TODO 5: Add notification feature 
     }
 }
